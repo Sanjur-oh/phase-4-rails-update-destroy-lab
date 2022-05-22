8763b3dc35672d12ed5f552b2176b1d8721c6520
Michael Franco <franco.luis.michael@gmail.com>
2022-05-22 14:33:59 -0500

    commit tree 6778d98b5ab63bea15058f845c18a0b8656c3458

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+    end
+  end
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+
+
+
+
 end
